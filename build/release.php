576f0349269dd055edf0730f5c299144f086b956
Josh Stoik <josh@blobfolio.com>
2017-07-30 11:48:17 -0700

    build release zip too

--- build/release.php
+++ build/release.php
@@ -14,8 +14,15 @@
 define('BUILD_DIR', dirname(__FILE__) . '/');
 define('SKEL_DIR', BUILD_DIR . 'skel/');
 define('PLUGIN_BASE', dirname(BUILD_DIR) . '/trunk/');
-define('RELEASE_BASE', dirname(BUILD_DIR) . '/wp-cli-musty/');
-define('RELEASE_SOURCE', RELEASE_BASE . 'opt/musty/');
+define('DEB_BASE', dirname(BUILD_DIR) . '/wp-cli-musty/');
+define('DEB_SOURCE', DEB_BASE . 'opt/musty/');
+define('RELEASE_ZIP', dirname(BUILD_DIR) . '/release/musty.zip');
+define('RELEASE_JSON', dirname(BUILD_DIR) . '/release/musty.json');
+define('MUSTY_JSON', '{
+  "version": "%VERSION%",
+  "download_link": "https:\/\/raw.githubusercontent.com\/Blobfolio\/musty\/master\/release\/musty.zip"
+}
+');
 
 
 
@@ -36,34 +43,77 @@ echo "\n";
 echo "+ Copying the source.\n";
 
 // Delete the release base if it already exists.
-if (file_exists(RELEASE_BASE)) {
-	shell_exec('rm -rf ' . escapeshellarg(RELEASE_BASE));
+if (file_exists(DEB_BASE)) {
+	shell_exec('rm -rf ' . escapeshellarg(DEB_BASE));
 }
 
 // Copy the debian stuff.
-shell_exec('cp -aR ' . escapeshellarg(SKEL_DIR) . ' ' . escapeshellarg(RELEASE_BASE));
-$tmp = @file_get_contents(RELEASE_BASE . 'DEBIAN/control');
+shell_exec('cp -aR ' . escapeshellarg(SKEL_DIR) . ' ' . escapeshellarg(DEB_BASE));
+$tmp = @file_get_contents(DEB_BASE . 'DEBIAN/control');
 $tmp = str_replace('%VERSION%', RELEASE_VERSION, $tmp);
-@file_put_contents(RELEASE_BASE . 'DEBIAN/control', $tmp);
+@file_put_contents(DEB_BASE . 'DEBIAN/control', $tmp);
 
 // Copy the trunk.
-mkdir(RELEASE_BASE . 'opt', 0755, true);
-shell_exec('cp -aR ' . escapeshellarg(PLUGIN_BASE) . ' ' . escapeshellarg(RELEASE_SOURCE));
+mkdir(DEB_BASE . 'opt', 0755, true);
+shell_exec('cp -aR ' . escapeshellarg(PLUGIN_BASE) . ' ' . escapeshellarg(DEB_SOURCE));
 
 
 
 echo "+ Cleaning the source.\n";
-unlink(RELEASE_SOURCE . 'Gruntfile.js');
-unlink(RELEASE_SOURCE . 'package.json');
-shell_exec('rm -rf ' . escapeshellarg(RELEASE_SOURCE . 'node_modules/'));
-shell_exec('find ' . escapeshellarg(RELEASE_BASE) . ' -name ".gitignore" -type f -delete');
+unlink(DEB_SOURCE . 'Gruntfile.js');
+unlink(DEB_SOURCE . 'package.json');
+shell_exec('rm -rf ' . escapeshellarg(DEB_SOURCE . 'node_modules/'));
+shell_exec('find ' . escapeshellarg(DEB_BASE) . ' -name ".gitignore" -type f -delete');
+
+
+
+// We can compress the Blobfolio vendor files.
+echo "+ Compressing files.\n";
+$dir = new RecursiveIteratorIterator(
+	new RecursiveDirectoryIterator(DEB_SOURCE . 'lib/vendor/blobfolio/', RecursiveDirectoryIterator::SKIP_DOTS)
+);
+foreach ($dir as $file) {
+	if (preg_match('/\.php$/i', $file)) {
+		@file_put_contents($file, php_strip_whitespace($file));
+	}
+}
 
 
 
 echo "+ Fixing permissions.\n";
-shell_exec('find ' . escapeshellarg(RELEASE_BASE) . ' -type d -print0 | xargs -0 chmod 755');
-shell_exec('find ' . escapeshellarg(RELEASE_BASE) . ' -type f -print0 | xargs -0 chmod 644');
+shell_exec('find ' . escapeshellarg(DEB_BASE) . ' -type d -print0 | xargs -0 chmod 755');
+shell_exec('find ' . escapeshellarg(DEB_BASE) . ' -type f -print0 | xargs -0 chmod 644');
 
 
+
+echo "+ Packaging Zip.\n";
+
+// The JSON is easy.
+@file_put_contents(RELEASE_JSON, str_replace('%VERSION%', RELEASE_VERSION, MUSTY_JSON));
+
+// The zip is a little more annoying.
+if (@file_exists(RELEASE_ZIP)) {
+	@unlink(RELEASE_ZIP);
+}
+
+$files = array();
+$dir = new RecursiveIteratorIterator(
+	new RecursiveDirectoryIterator(DEB_SOURCE, RecursiveDirectoryIterator::SKIP_DOTS)
+);
+foreach ($dir as $file) {
+	$files[] = (string) $file;
+}
+
+$zip = new ZipArchive();
+if ($zip->open(RELEASE_ZIP, ZipArchive::CREATE) === true) {
+	foreach($files as $file) {
+		$zip->addfile(
+			$file,
+			preg_replace('/^' . preg_quote(DEB_SOURCE, '/') . '/u', '', $file)
+		);
+	}
+	$zip->close();
+}
 
 echo "\nDone!.\n";
+exit(0);
